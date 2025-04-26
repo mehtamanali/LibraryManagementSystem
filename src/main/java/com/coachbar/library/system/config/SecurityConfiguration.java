deb13ef89c18572991fbfb25a80e9f557cff8999
mehtamanali <mehtamanali1298@gmail.com>
2025-04-26 23:08:03 +0530

    security configuration changes

--- src/main/java/com/coachbar/library/system/config/SecurityConfiguration.java
+++ src/main/java/com/coachbar/library/system/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.coachbar.library.system.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -15,8 +16,17 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize ->authorize
-                        .requestMatchers("/swagger-ui/**").hasRole("USER")
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).authenticated()
+                        .requestMatchers(HttpMethod.GET, "/book/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/book").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/book/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/book/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 ).httpBasic(Customizer.withDefaults());
 
@@ -27,10 +37,17 @@ public class SecurityConfiguration {
     public InMemoryUserDetailsManager userDetailsService() {
         UserDetails user = User.withDefaultPasswordEncoder()
                 .username("user")
-                .password("password")
+                .password("user")
                 .roles("USER")
                 .build();
-        return new InMemoryUserDetailsManager(user);
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 
 }
